88bd7c50d6502bcdf6f525bbd2147378e28bab29
trevorah <a.trevorah@gmail.com>
2013-03-05 23:33:10 +0000

    initial commit

---
+++ public/generator.js
@@ -0,0 +1,34 @@
+
+var vowels = ["a","e","i","o","u"];
+var consanants = ["b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","w","x","y","z"];
+
+var length = 3 + Math.floor(Math.random()*7);
+var name = "";
+
+for(var i = 0; i < length; i++) {
+	var letter = "";
+	if(i % 2 == 0) {
+		letter = vowels[Math.floor(Math.random()*vowels.length)];
+	} else {
+		letter = consanants[Math.floor(Math.random()*consanants.length)];
+	}
+	name = name + letter;
+}
+
+$("#username").text(name);
+
+$.get("github/" + name, function(data) {
+	if(data == "free") {
+		$("#github").attr("class", "icon-ok");
+	} else {
+		$("#github").attr("class", "icon-remove");
+	}
+});
+
+$.get("twitter/" + name, function(data) {
+	if(data == "free") {
+		$("#twitter").attr("class", "icon-ok");
+	} else {
+		$("#twitter").attr("class", "icon-remove");
+	}
+});
---
+++ server.js
@@ -0,0 +1,28 @@
+var request = require("request");
+var express = require('express');
+
+var app = express();
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/github/:username', function(req, res) {
+	request('http://www.github.com/' + req.params.username, function (error, response, body) {
+		if(!error && response.statusCode == 200) {
+			res.send('taken');
+		} else {
+			res.send('free');
+		}
+	});
+});
+
+app.get('/twitter/:username', function(req, res) {
+	request('http://www.twitter.com/' + req.params.username, function (error, response, body) {
+		if(!error && response.statusCode == 200) {
+			res.send('taken');
+		} else {
+			res.send('free');
+		}
+	});
+});
+
+app.listen(process.env.PORT || 8080);
